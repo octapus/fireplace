aa84797c0a6cebdf4cfa9b194cfb94c885051411
8ctapus <octapus@users.noreply.github.com>
2014-04-23 22:46:16 -0400

    fix options on findFetchQuery

--- packages/fireplace/lib/store.js
+++ packages/fireplace/lib/store.js
@@ -176,7 +176,7 @@ FP.Store = Ember.Object.extend({
     if (options.path) {
       reference = model.buildFirebaseRootReference(this).child(options.path);
     } else {
-      reference = model.buildFirebaseReference(this, query);
+      reference = model.buildFirebaseReference(this, options);
     }
 
     return this.findFetchCollectionByReference(model, reference, query, options, returnPromise);
